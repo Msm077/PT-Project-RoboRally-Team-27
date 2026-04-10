142c54980d664826c123f064134f30375b2d835d
Msm <mohammadsamehm07@gmail.com>
2026-04-10 22:51:17 +0200

    Draw Triangle

--- Output.cpp
+++ Output.cpp
@@ -156,8 +156,6 @@ void Output::DrawTriangle(int triangleCenterX, int triangleCenterY, int triangle
 {
 	int x1, y1, x2, y2, x3, y3;
 
-	///TODO: Calculate the coordiantes of the 3 vertices of the triangle based on the passed parameters
-
 	if (direction == UP)
 	{
 		x1 = triangleCenterX - triangleWidth / 2;
@@ -167,8 +165,33 @@ void Output::DrawTriangle(int triangleCenterX, int triangleCenterY, int triangle
 		x3 = triangleCenterX;
 		y3 = triangleCenterY - triangleHeight / 2;
 	}
-	///TODO: Continue the implementation
-	
+	else if (direction == DOWN)
+	{
+		x1 = triangleCenterX - triangleWidth / 2;
+		y1 = triangleCenterY - triangleHeight / 2;
+		x2 = triangleCenterX + triangleWidth / 2;
+		y2 = triangleCenterY - triangleHeight / 2;
+		x3 = triangleCenterX;
+		y3 = triangleCenterY + triangleHeight / 2;
+	}
+	else if (direction == RIGHT)
+	{
+		x1 = triangleCenterX - triangleHeight / 2;
+		y1 = triangleCenterY - triangleWidth / 2;
+		x2 = triangleCenterX - triangleHeight / 2;
+		y2 = triangleCenterY + triangleWidth / 2;
+		x3 = triangleCenterX + triangleHeight / 2;
+		y3 = triangleCenterY;
+	}
+	else if (direction == LEFT)
+	{
+		x1 = triangleCenterX + triangleHeight / 2;
+		y1 = triangleCenterY - triangleWidth / 2;
+		x2 = triangleCenterX + triangleHeight / 2;
+		y2 = triangleCenterY + triangleWidth / 2;
+		x3 = triangleCenterX - triangleHeight / 2;
+		y3 = triangleCenterY;
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
