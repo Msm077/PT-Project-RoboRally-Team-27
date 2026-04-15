a4e1d48aa9227a567ac14f20ff95c69875c378e0
AbdelrahmanAbdelwahab1 <142083653+AbdelrahmanAbdelwahab1@users.noreply.github.com>
2026-04-16 00:17:53 +0200

    Refactor Input.cpp: Clean up GetInteger function
    Removed commented-out test code and cleaned up exception handling in GetInteger function.

--- Input.cpp
+++ Input.cpp
@@ -54,19 +54,6 @@ int Input::GetInteger(Output* pO) const
 	//       using function GetString() defined above and function stoi()
 
 	// BY ME ME
-	/*
-	to test
-
-	pIn->GetCellClicked();
-
-	pOut->PrintMessage("Please type an integer and press ENTER:");
-
-	// 1. Store the returned value in a variable
-	int enteredInt = pIn->GetInteger(pOut);
-
-
-	*/
-
 
 
 	/*
@@ -86,23 +73,11 @@ int Input::GetInteger(Output* pO) const
 	string input = GetSrting(pO);
 	try {
 		int integer = stoi(input);
-		cout << "seco integer" << integer << endl;
 		return integer;
 	}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-	catch(...){
-<<<<<<< Updated upstream
-		cout << input <<"that's not an integer " << endl;
-=======
-=======
+
 	catch (...) {
->>>>>>> Stashed changes
-=======
-	catch (...) {
->>>>>>> Stashed changes
 		pO->PrintMessage("This is not an Integer");
->>>>>>> Stashed changes
 		return -1;
 	}
 
@@ -259,21 +234,11 @@ CellPosition Input::GetCellClicked() const
 
 
 			int cell_width = UI.width / 11;
-<<<<<<< Updated upstream
 			int cell_height = (UI.height - (UI.CommandsBarHeight + UI.StatusBarHeight + UI.ToolBarHeight)) / 5;
 
-=======
-			int cell_height = (UI.height - (UI.CommandsBarHeight + UI.StatusBarHeight + UI.ToolBarHeight )) / 5 ;
-				
->>>>>>> Stashed changes
 
 			int H_cell = x / cell_width;
-<<<<<<< Updated upstream
-			int V_cell = y / cell_height;
-			int V_cell = y / cell_height-1;
-=======
 			int V_cell = y / cell_height -1;
->>>>>>> Stashed changes
 
 			if (H_cell > 10) {
 				cellPos.SetHCell(-1);
@@ -295,8 +260,6 @@ CellPosition Input::GetCellClicked() const
 
 
 	}
-	cout << "sayed cell clicked" << cellPos.VCell()<<"  h cell" << cellPos.HCell() << endl;
-
 
 	return cellPos;
 }
