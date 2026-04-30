bbe46cfa06bfada69b6a9ee54d661c3fecaba2fc
AbdelrahmanAbdelwahab1 <142083653+AbdelrahmanAbdelwahab1@users.noreply.github.com>
2026-04-30 22:36:57 +0300

    Update SelectCommandsAction.cpp

---
+++ SelectCommandsAction.cpp
@@ -0,0 +1,109 @@
+#include "SelectCommandsAction.h"
+#include "Player.h"
+#include "Input.h"
+#include<iostream>
+#include"Grid.h"
+#include "GameState.h"
+using namespace std;
+
+SelectCommandsAction::SelectCommandsAction(ApplicationManager* pApp) : Action(pApp)
+{
+	// every action needs to access the game's tools to work. 
+	//cuz i passed pApp to the parent Action class, 
+	// i can now use pManager inside other functions to reach everything else in the game
+    numSelected = 0; // Initializing our counter
+}
+
+
+// mapping fn used in "ReadActionParameters" fn to make the message for the user clearer
+
+string GetCommandName(Command cmd)
+{
+	switch (cmd)
+	{
+	case MOVE_FORWARD_ONE_STEP:     return "Move Forward 1 Step";
+	case MOVE_BACKWARD_ONE_STEP:    return "Move Backward 1 Step";
+	case MOVE_FORWARD_TWO_STEPS:    return "Move Forward 2 Steps";
+	case MOVE_BACKWARD_TWO_STEPS:   return "Move Backward 2 Steps";
+	case MOVE_FORWARD_THREE_STEPS:  return "Move Forward 3 Steps";
+	case MOVE_BACKWARD_THREE_STEPS: return "Move Backward 3 Steps";
+	case ROTATE_CLOCKWISE:          return "Rotate Clockwise";
+	case ROTATE_COUNTERCLOCKWISE:   return "Rotate Counter-Clockwise";
+	default:                        return "No Command";
+	}
+}
+
+void SelectCommandsAction::ReadActionParameters()
+{
+	// 1st part
+	// Retrieve the Grid from ApplicationManager to access the board and game data.
+	// Get Input and Output pointers to handle all user interaction and drawing through the graphics library.
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	// 2nd part determine the current player & his health  to update his data
+	Player* pCurrentPlayer = pManager->GetGameState()->GetCurrentPlayer();
+	int health = pManager->GetGameState()->GetCurrentPlayer()->GetHealth();
+
+
+	// 3rd part get min(5, health)
+	int min = 5;
+	if (health < 5) {
+		min = health;
+	}
+	// 4th part putting the user in the situation
+	pOut->PrintMessage(" choose " + to_string(min) + " commands ");
+
+	// 5th part clear the player's former commands
+	pCurrentPlayer->ClearSavedCommands();
+
+	// 6th part: loop to get the selected commands using the input:: GetSelectedCommandIndex()
+	for (int i = 0; i < min; i++) {
+
+
+		int commandIndex = -1;
+		// loop until a valid index is entered
+
+		while (commandIndex >= COMMANDS_COUNT ||  commandIndex < 0 ){
+			commandIndex = pIn->GetSelectedCommandIndex();
+
+			if (commandIndex == -1) {
+				pOut->PrintMessage("invalid click! Please select a command from the bar.");
+			}
+		}
+
+		// map the index returned from "pIn->GetSelectedCommandIndex()" with it appropriate command
+		// use "AddSavedCommand"  to add the command
+
+		Command selectedCmd = static_cast <Command>(commandIndex);
+
+		pCurrentPlayer->AddSavedCommand(selectedCmd);
+
+		string cmdName = GetCommandName(selectedCmd);
+
+		pOut->PrintMessage(" slot " + to_string(i + 1) + ": " + cmdName + " selected.");
+
+	}
+
+	
+}
+
+void SelectCommandsAction::Execute()
+{
+
+
+	// 1. Get the commands
+	ReadActionParameters();
+
+	// notify the user
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	pGrid->GetOutput()->PrintMessage("Commands saved! Ready to move.");
+}
+
+SelectCommandsAction::~SelectCommandsAction()
+{
+}
