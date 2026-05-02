25adeed49bf7ffbb3d3cba5c8bbac6759e63506c
AhmedDokhan27807 <azaki3868@gmail.com>
2026-05-02 20:49:24 +0300

    Update AddDangerZoneAction.cpp

--- AddDangerZoneAction.cpp
+++ AddDangerZoneAction.cpp
@@ -61,20 +61,7 @@ void AddDangerZoneAction::Execute()
 	ReadActionParameters();
 
 	// Create a belt object with the parameters read from the user
-	//DangerZone* pDZ = new DangerZone(dangerZonePos);
-
-	//Grid* pGrid = pManager->GetGrid(); // We get a pointer to the Grid from the ApplicationManager
-
-
-	//bool added = pGrid->AddObjectToCell(pDZ);
-
-	//// if the GameObject cannot be added
-	//if (!added)
-	//{
-	//	// Print an appropriate message
-	//	pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
-	//}
-	//// Here, the belt is created and added to the GameObject of its Cell, so we finished executing the AddDangerZoneAction
+	
 
 }
 
