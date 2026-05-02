205e0fec05a4b7420560c77cc8e1409c8a7aa3cb
AhmedDokhan27807 <azaki3868@gmail.com>
2026-05-02 20:01:14 +0300

    Update AddDangerZoneAction.cpp

--- AddDangerZoneAction.cpp
+++ AddDangerZoneAction.cpp
@@ -24,7 +24,31 @@ void AddDangerZoneAction::ReadActionParameters()
 
 
 	///TODO: Make the needed validations on the read parameters
+    //AA
+	if (!dangerZonePos.IsValidCell())
+	{
+		pGrid->PrintErrorMessage("Error: Invalid cell position! Click to continue...");
+		dangerZonePos = CellPosition();
+		pOut->ClearStatusBar();
+		return;
+	}
+	
+	if (dangerZonePos.GetCellNum() == 1)
+	{
+		pGrid->PrintErrorMessage("Error: Cannot place danger zone in start cell (Cell 1)! Click to continue...");
+		dangerZonePos = CellPosition();
+		pOut->ClearStatusBar();
+		return;
+	}
 
+	
+	if (pGrid->GetGameObjectFromCell(dangerZonePos) != nullptr)
+	{
+		pGrid->PrintErrorMessage("Error: Cell already has an object! Click to continue...");
+		dangerZonePos = CellPosition();
+		pOut->ClearStatusBar();
+		return;
+	}
 
 
 	// Clear messages
