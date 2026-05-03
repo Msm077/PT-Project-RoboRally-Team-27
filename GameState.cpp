396bf5c50ceb426e40b5dbada182bc6682a2ccc2
Eyad Ahmed <eyaddesoky42@gmail.com>
2026-05-03 22:23:13 +0300

    Get Current player and get player
    in the game state

--- GameState.cpp
+++ GameState.cpp
@@ -34,13 +34,13 @@ GameState::~GameState()
 Player* GameState::GetCurrentPlayer() const
 {
 	///TODO: Return the player whose turn it is
-	return PlayerList[0]; // wrong -- update this
+	return PlayerList[currPlayerNumber];
 }
 
 Player* GameState::GetPlayer(int playerNum) const
 {
 	///TODO: Return the player with the given player number
-	return PlayerList[0]; // wrong -- update this
+	return PlayerList[playerNum]; 
 }
 
 // ========== Turn Management ==========
